956d4348e632b084ab33cd3c51d0fbe34d8520b7
Scott Busche <swbusche@gmail.com>
2016-12-21 10:05:57 -0600

    All the updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    TeslaBatteryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { BatteryService } from './tesla-battery.service';
+
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
+import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
+import { TeslaWheelsComponent } from './components/tesla-wheels/tesla-wheels.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    TeslaBatteryComponent,
+    TeslaCarComponent,
+    TeslaStatsComponent,
+    TeslaCounterComponent,
+    TeslaClimateComponent,
+    TeslaWheelsComponent
+  ],
+  exports: [
+    TeslaBatteryComponent
+  ],
+  providers: [
+    BatteryService
+  ]
+})
+export class TeslaBatteryModule { }
